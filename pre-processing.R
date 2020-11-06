90bae4f1712cd61cee88e4b1403ac94ace9c4b4e
Ofer <Oferikad@gmail.com>
2020-11-06 13:49:13 +0100

    d

--- pre-processing.R
+++ pre-processing.R
@@ -1,1 +1,10 @@
-#data cleaning code
+#load data
+library(haven)
+library(caret)
+library(tidyverse)
+Uganda_Data <- read_dta("~/Desktop/Master thesis/MLhetrogeneouseffects/data/Uganda ELA Panel wide.dta")
+View(Uganda_Data)
+
+
+
+
